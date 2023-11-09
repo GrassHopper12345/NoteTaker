9805fb943322977fc8a331d35a4a389d622124bc
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-11-09 16:21:53 -0600

    update to apiroutes file. put cael backs around const notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,21 +1,20 @@
 const router = require('express').Router();
-const notes = require('../db/db.json');
-// const {v4: uuid} = require('uuid');
+const {notes} = require('../db/db.json');
+const {v4: uuid} = require('uuid');
 const {createNewNotes, updateDb} = require('../db/notes');
 
 router.get('/notes', (req, res) => {
-    console.log(notes);
     res.json(notes)
 });
 
 router.post('/notes', (req, res) => {
-    // req.body = uuid.v4();
-    // const newNotes = createNewNotes(req.body, notes);
-    createNewNotes(req.body, notes);
-    res.redirect('/notes');
+    req.body.id = uuid.v4();
+    const newNotes = createNewNotes(req.body, notes);
+    // createNewNotes(req.body, notes);
+    res.redirect(newNotes);
 });
 
-router.delete('/notes', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     const params = req.params
     updateDb(params, notes);
     res.redirect('');
