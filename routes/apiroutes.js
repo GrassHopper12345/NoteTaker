59ba55bb0a28543aa363dc442fec339a6dde2dee
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-11-09 21:19:26 -0600

    update to apiroutes file with get, post, and delete

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,36 +1,82 @@
 const router = require('express').Router();
 const fs = require('fs');
-// const {notes} = require('../db/db.json');
-const {v4: uuid} = require('uuid');
+const uuid = require('uuid');
 const {createNewNotes, updateDb} = require('../db/notes');
 
 
 router.get('/notes', (req, res) => {
-    // fs.readFile('db.json', 'utf8', (err, data) =>{
-    //     if(err) {
-    //         console.error(err);
-    //         return res.status(500).json({message: err.message});
-    //     } else {
-    //         const savedNotes = JSON.parse(data);
-    //         console.log(savedNotes);
-    //     }
-    // })
-    const data = fs.readFileSync('db.json', 'utf8');
+    const data = fs.readFileSync('./db/db.json', 'utf-8');
     const savedNotes = JSON.parse(data);
     res.json(savedNotes);
 });
 
-// router.post('/notes', (req, res) => {
-//     req.body.id = uuid.v4();
-//     const newNotes = createNewNotes(req.body, notes);
-//     // createNewNotes(req.body, notes);
-//     res.redirect(newNotes);
-// });
+router.post('/notes', (req, res) => {
+  const newNoteId = uuid.v4();
 
-// router.delete('api/notes/:id', (req, res) => {
-//     const params = req.params
-//     updateDb(params, notes);
-//     res.redirect('');
-// });
+  // Set the id property of the request body to the generated id
+  req.body.id = newNoteId;
+
+  // Read the contents of the db.json file
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    // Parse the JSON data into an array of saved notes
+    const savedNotes = JSON.parse(data);
+
+    // Add the new note to the array of saved notes
+    savedNotes.push(req.body);
+
+    // Write the updated array of saved notes back to the db.json file
+    fs.writeFile('./db/db.json', JSON.stringify(savedNotes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      // Return the updated array of saved notes as a JSON response
+      res.json(savedNotes);
+    });
+  });
+});
+
+router.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+  
+    // Read the contents of the db.json file
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+  
+      // Parse the JSON data into an array of saved notes
+      let savedNotes = JSON.parse(data);
+  
+      // Find the index of the note with the matching id
+      const noteIndex = savedNotes.findIndex(note => note.id === noteId);
+  
+      // If the note is found, remove it from the array
+      if (noteIndex !== -1) {
+        savedNotes.splice(noteIndex, 1);
+  
+        // Write the updated array of saved notes back to the db.json file
+        fs.writeFile('./db/db.json', JSON.stringify(savedNotes), (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
+  
+          // Return a success message as a JSON response
+          res.json({ message: 'Note deleted successfully' });
+        });
+      } else {
+        // If the note is not found, return a 404 Not Found response
+        res.status(404).json({ error: 'Note not found' });
+      }
+    });
+  });
 
 module.exports = router;
